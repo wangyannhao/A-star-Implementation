0e6fe28c935e1a7582e4da1f148aa386a5c31b86
wangyannhao <wangyannhao@gmail.com>
2016-11-02 00:36:36 -0400

    Map search with a star uploaded.

---
+++ aStarAlg/src/aStarAlg/Frame.java
@@ -0,0 +1,36 @@
+package aStarAlg;
+import javax.swing.JFrame;
+
+
+
+public class Frame extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//Draw s;
+	
+	public Frame()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(160*5+100,120*5+100);
+		setResizable(false);
+		setTitle("Map");
+
+	}
+	
+	public void init()
+	{
+//		setLocationRelativeTo(null);
+//		
+//		//setLayout(new GridLayout(1,1,0,0));
+//		
+//		s = new Draw();
+//		
+//		add(s);
+//		
+//		setVisible(true);
+	}
+	
+
+}
